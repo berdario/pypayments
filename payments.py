79b45b20bb1a4ce61ead1d57a271fa93b8c89733
Dario Bertini <berdario@gmail.com>
2016-02-24 01:07:39 +0000

    first commit for HTTP payments api with tests

---
+++ payments.py
@@ -0,0 +1,66 @@
+from decimal import Decimal
+
+from faker import Faker
+import hug
+import sqlalchemy as sql
+
+@sql.event.listens_for(sql.engine.Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
+sqlite = sql.create_engine('sqlite:///:memory:', echo=True)
+
+meta = sql.MetaData()
+accounts = sql.Table('account', meta,
+    sql.Column('id', sql.Integer, primary_key=True),
+    sql.Column('name', sql.String),
+    sql.Column('email', sql.String),
+    sql.Column('balance', sql.Numeric, sql.CheckConstraint('balance>=0'), nullable=False),
+)
+
+transactions = sql.Table('transactions', meta,
+    sql.Column('id', sql.Integer, primary_key=True),
+    sql.Column('source_id', sql.Integer, sql.ForeignKey(accounts.c.id), index=True, nullable=False),
+    sql.Column('recipient_id', sql.Integer, sql.ForeignKey(accounts.c.id), index=True, nullable=False),
+    sql.Column('amount', sql.Numeric, sql.CheckConstraint('amount>=0')),
+)
+
+
+meta.create_all(sqlite)
+meta.bind = sqlite
+
+fake = Faker()
+accounts.insert().execute([{'balance': 200,
+                            'name': fake.user_name(),
+                            'email': fake.email()} for _ in range(25)])
+
+
+@hug.get('/')
+def main():
+    return {accnt.id: accnt for accnt in accounts.select().execute()}
+
+@hug.get('/account')
+def account(account_id: int):
+    return list(transactions.select().where((transactions.c.source_id == account_id) | 
+                                            (transactions.c.recipient_id == account_id)).execute())
+
+    
+def update_balance(account_id: int, delta: int):
+    new_balance = sql.select([accounts.c.balance + delta]).where(accounts.c.id == account_id)
+    return accounts.update().where(accounts.c.id == account_id).values(balance=new_balance)
+
+
+@hug.post('/pay')
+def pay(source: int, recipient: int, amount: Decimal, connection=None):
+    connection = connection or sqlite.connect()
+    with connection.begin():
+        connection.execute(update_balance(source, -amount))
+        connection.execute(update_balance(recipient, +amount))
+        # inserting into transactions is not only needed for /account
+        # but it also checks the id validity and a positive amount
+        connection.execute(transactions.insert().values(source_id=source,
+                                                         recipient_id=recipient,
+                                                         amount=amount))
+
---
+++ test_payments.py
@@ -0,0 +1,56 @@
+from pytest import yield_fixture, raises
+from sqlalchemy.exc import IntegrityError
+
+from payments import sqlite, main as all_accounts, account, pay
+
+# in these tests we rely on the builtin 25 fake users created in the payments module
+
+@yield_fixture
+def connection():
+    conn = sqlite.connect()
+    with conn.begin() as transaction:
+        yield conn
+        transaction.rollback()
+
+def test_successful_payment(connection):
+    pay(source=1, recipient=2, amount=30, connection=connection)
+    accts = all_accounts()
+    assert accts[1].balance == 170
+    assert accts[2].balance == 230
+
+def test_successful_payment2(connection):
+    """This test verifies that tests changes are properly rollbacked"""
+    accts = all_accounts()
+    assert accts[1].balance == 200
+    pay(1, 2, 30, connection)
+    accts = all_accounts()
+    assert accts[1].balance == 170
+    assert accts[2].balance == 230
+    
+def test_balance_can_go_to_zero(connection):
+    pay(1, 2, 200, connection)
+    assert all_accounts()[1].balance == 0
+    
+def test_transaction_log(connection):
+    pay(1, 2, 30, connection)
+    pay(2, 3, 30, connection)
+    assert len(account(2)) == 2
+    
+def test_give_back_money(connection):
+    before = all_accounts()
+    pay(1, 2, 30, connection)
+    pay(2, 1, 30, connection)
+    after = all_accounts()
+    assert before == after
+
+def test_negative_balance(connection):
+    with raises(IntegrityError):
+        pay(1, 2, 200.01, connection)
+    
+def test_nonexisting_account_id(connection):
+    with raises(IntegrityError):
+        pay(101, 2, 10, connection)
+    
+def test_negative_amount(connection):
+    with raises(IntegrityError):
+        pay(1, 2, -1, connection)
