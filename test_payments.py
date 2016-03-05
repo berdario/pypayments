8d9a89db0d027f307edaee75dee650378e678ef6
Dario Bertini <berdario@gmail.com>
2016-03-05 22:39:52 +0000

    Add tests with mocks

--- test_payments.py
+++ test_payments.py
@@ -1,6 +1,9 @@
+from unittest.mock import patch, call, MagicMock, ANY
+
 from pytest import yield_fixture, raises
 from sqlalchemy.exc import IntegrityError
 
+import payments
 from payments import sqlite, main as all_accounts, account_transactions, pay
 
 # in these tests we rely on the builtin 25 fake users created in the payments module
@@ -54,3 +57,28 @@ def test_nonexisting_account_id(connection):
 def test_negative_amount(connection):
     with raises(IntegrityError):
         pay(1, 2, -1, connection)
+        
+        
+def test_all_accounts():
+    with patch.object(payments, 'accounts') as accounts:
+        all_accounts()
+    assert accounts.mock_calls[:2] == [call.select(),
+                                       call.select().execute()]
+                                       
+def test_account_transactions():
+    with patch.object(payments, 'transactions') as transactions:
+        account_transactions(2)
+    assert transactions.mock_calls[3:5] == [call.select().where(ANY),
+                                            call.select().where(ANY).execute()]
+                                            
+def test_pay():
+    m = MagicMock()
+    pay(1, 2, 30, connection=m)
+    queries = [str(c[1][0]) for c in m.execute.mock_calls]
+    assert queries == ['UPDATE account SET balance=(SELECT account.balance + ? AS anon_1 \n'
+                       'FROM account \n'
+                       'WHERE account.id = ?) WHERE account.id = ?',
+                       'UPDATE account SET balance=(SELECT account.balance + ? AS anon_1 \n'
+                       'FROM account \n'
+                       'WHERE account.id = ?) WHERE account.id = ?',
+                       'INSERT INTO transactions (source_id, recipient_id, amount) VALUES (?, ?, ?)']
